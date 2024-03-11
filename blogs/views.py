58032ffdbcbaaf17d2b06a62b7912daf3e646452
azeemharys1986 <azeemharys@gmail.com>
2024-03-11 10:49:34 +0530

    xx

--- blogs/views.py
+++ blogs/views.py
@@ -18,6 +18,7 @@ def signup(request):
         form = UserForm(request.POST)
         if form.is_valid():
             form.save()
+            
             return HttpResponseRedirect('/login') # Redirect to login page after signup
     else:
         form = UserForm()
